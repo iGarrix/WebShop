1e4645c89c4372ab1dd987da535777bf56824d75
Tarn_6txg@student.itstep.org <Tarn_6txg@student.itstep.org>
2021-06-30 15:06:58 +0300

    Saved

--- Shop/Controllers/HomeController.cs
+++ Shop/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Shop.Data;
 using Shop.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,15 +16,32 @@ namespace Shop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _db = db;
+            this._webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
-            return View();
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            string uploads = webRootPath + ENV.imagepass;
+
+            IEnumerable<Product> products = _db.Products.Include(u => u.Category);
+            foreach (var item in products)
+            {
+                if (item.Image != null)
+                {
+                    string name = item.Image;
+                    string final = Path.Combine(uploads, name);
+                    item.Image = final;
+                }
+            }
+            return View(products);
         }
 
         public IActionResult Privacy()
--- Shop/Controllers/ProductsController.cs
+++ Shop/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,7 @@ using Shop.Models;
 using Shop.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,9 +17,11 @@ namespace Shop.Controllers
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public ProductsController(ApplicationDbContext db)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductsController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             this._db = db;
+            this._webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -25,7 +29,6 @@ namespace Shop.Controllers
             IEnumerable<Product> products = _db.Products.Include(u => u.Category);
             return View(products);
         }
-        bool iscreate = false;
 
         public IActionResult Upsert(int? id)
         {
@@ -36,7 +39,6 @@ namespace Shop.Controllers
             };
             if (id == null)
             {
-                iscreate = true;
                 return View(productvm);
             }
             else
@@ -46,29 +48,64 @@ namespace Shop.Controllers
                 {
                     return NotFound();
                 }
-                iscreate = false;
                 return View(productvm);
             }
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Product product)
+        public IActionResult Upsert(ProductVM productVM)
         {
             if (ModelState.IsValid)
             {
-                if (iscreate)
-                {
-                    _db.Products.Add(product);
-                    _db.SaveChanges();
+                var files = HttpContext.Request.Form.Files;
+                string webRootPath = _webHostEnvironment.WebRootPath;
+
+                string uploads = webRootPath + ENV.imagepass;
+                string FileName = Guid.NewGuid().ToString();
+                string FileExt = Path.GetExtension(files[0].FileName);
+
+                if (productVM.Product.Id == 0)
+                {                 
+                    using (var fs = new FileStream(Path.Combine(uploads, FileName + FileExt), FileMode.Create))
+                    {
+                        files[0].CopyTo(fs);
+                    }
+
+                    productVM.Product.Image = FileName + FileExt;
+                    _db.Products.Add(productVM.Product);
                 }
                 else
                 {
-                    _db.Products.Update(product);
-                    _db.SaveChanges();
+                    var formObj = _db.Products.AsNoTracking().FirstOrDefault(u => u.Id == productVM.Product.Id);
+                    if (files.Count > 0)
+                    {
+                        var oldFile = Path.Combine(uploads, formObj.Image);
+                        if (System.IO.File.Exists(oldFile))
+                        {
+                            System.IO.File.Delete(oldFile);
+                        }
+                        using (var fs = new FileStream(Path.Combine(uploads, FileName + FileExt), FileMode.Create))
+                        {
+                            files[0].CopyTo(fs);
+                        }
+                        productVM.Product.Image = FileName + FileExt;
+                    }
+                    else
+                    {
+                        productVM.Product.Image = formObj.Image;
+
+                    }
+                    _db.Products.Update(productVM.Product);
                 }
+                _db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View();
+            productVM.CategorySelectList = _db.Category.Select(s => new SelectListItem
+            {
+                Text = s.Name,
+                Value = s.Id.ToString()
+            });
+            return View(productVM);
         }
 
         public IActionResult Delete(int? id)
@@ -93,6 +130,14 @@ namespace Shop.Controllers
             if (products == null)
             {
                 return NotFound();
+            }
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            string uploads = webRootPath + ENV.imagepass;
+            string path = Path.Combine(uploads, products.Image);
+
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
             }
 
             _db.Products.Remove(products);
