8a00e903b42ced70519d9ce1f0be3c8e0c921f98
Tarn_6txg@student.itstep.org <Tarn_6txg@student.itstep.org>
2021-07-19 15:02:43 +0300

    Saved!

--- Shop/Controllers/CartController.cs
+++ Shop/Controllers/CartController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore;
 using Shop.Data;
 using Shop.Models;
@@ -8,8 +11,11 @@ using Shop.Models.ViewModels;
 using Shop.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace Shop.Controllers
@@ -18,10 +24,13 @@ namespace Shop.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
-
-        public CartController(ApplicationDbContext db)
+        private readonly IEmailSender _emailSender;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public CartController(ApplicationDbContext db, IEmailSender emailSender, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _emailSender = emailSender;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -39,6 +48,14 @@ namespace Shop.Controllers
             }
             return View(products);
         }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //[ActionName("Index")]
+        //public IActionResult IndexPost()
+        //{
+        //    return RedirectToAction(nameof(Order));
+        //}
 
         public IActionResult RemoveFromCart(int id)
         {
@@ -114,16 +131,43 @@ namespace Shop.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [ActionName("Order")]
-        public IActionResult OrderPost(ProductUserVM productUserVM)
+        public async Task<IActionResult> OrderPostAsync(ProductUserVM productUserVM)
         {
-            // send email 
+            // send email
+
+            var path_template = _webHostEnvironment.WebRootPath + Path.DirectorySeparatorChar.ToString()
+                + "teplates" + Path.DirectorySeparatorChar.ToString() + "orderproduct_template.html";
+
+            var subject = "New order";
+            string HTML_body = "";
+
+            using (StreamReader sr = System.IO.File.OpenText(path_template))
+            {
+                HTML_body = sr.ReadToEnd();
+            }
+
+            StringBuilder productsSB = new StringBuilder();
+
+            foreach (var item in productUserVM.ProductList)
+            {
+                productsSB.Append($"{item.Name}\t | \t{item.Price}$UAN");
+            }
+
+            string messageBody = string.Format(
+                HTML_body,
+                productUserVM.AppUser.Name, productUserVM.AppUser.Email, productUserVM.AppUser.PhoneNumber,
+                productsSB.ToString());
+
+
+            await _emailSender.SendEmailAsync(ENV.AdminEmail, subject, messageBody);
 
             return RedirectToAction(nameof(OrderConfirmarion));
+
         }
 
         public IActionResult OrderConfirmarion()
         {
-            //HttpContext.Session.Clear();
+            HttpContext.Session.Clear();
             return View();
         }
     }
--- Shop/ENV.cs
+++ Shop/ENV.cs
@@ -12,5 +12,7 @@ namespace Shop
 
         public static string AdminRole = "Admin";
         public static string CustomerRole = "Customer";
+
+        public static string AdminEmail = "garrixdit@gmail.com";
     }
 }
